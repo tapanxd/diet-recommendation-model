f7ab785203d6e10de40a1d038f7b4454a441d94f
Tapan Panchal <45334392+tapanxd@users.noreply.github.com>
2021-04-08 13:01:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
 from flask import Flask
-# from diet_Copy import *
+from diet_Copy import *
 app = Flask(__name__)
 @app.route('/')
 def predict():
     return "Demo page"
 
-# @app.route('/<string:a>/<string:w>/<string:h>/<string:p>')
-# def func2(a, w, h, p):
-#     pref = None
-#     if p == "Veg":
-#         pref = 0
-#     elif p == "Non-Veg":
-#         pref = 1
+@app.route('/<string:a>/<string:w>/<string:h>/<string:p>')
+def func2(a, w, h, p):
+    pref = None
+    if p == "Veg":
+        pref = 0
+    elif p == "Non-Veg":
+        pref = 1
 
-#     diet = main_func(a, w, pref, h)
-#     dietstr = "<br>".join(diet)
-#     return dietstr
+    diet = main_func(a, w, pref, h)
+    dietstr = "<br>".join(diet)
+    return dietstr
 
 if __name__ == "__main__":
     app.run(debug = True)
